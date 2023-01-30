adf5363387e1f31be4224292b0862a80af5b49d7
Steve-Mcl <sdmclaughlin@gmail.com>
2023-01-30 12:29:46 +0000

    default test to sqlite + file based db

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const YAML = require('yaml')
 
 // setup mock app with authentication endpoint
 // that provides auth token validation for testing
@@ -50,30 +51,41 @@ function authServer (config = {}) {
  * @param {number} config.port port
  * @param {string} config.base_url base url
  * @param {Object} config.context context settings
- * @param {'memory'|'sequelize'|'redis'} config.context.type context type (memory, sequelize, redis)
- * @param {*} config.context.quota context quota (max size in MB)
+ * @param {'memory'|'sequelize'|'redis'} config.context.type context type
+ * @param {number} config.context.quota context quota (max size in MB)
+ * @param {Object} config.context.options context options (depends on context type)
+ * @param {'sqlite'|'postgres'} config.context.options.type context options type
  * @returns @flowforge/file-server
  */
 async function setupFileServerApp (config = {}) {
-    process.env.FF_FS_TEST_CONFIG = `
-FLOWFORGE_HOME: ${config.home || process.cwd()}
-FLOWFORGE_PROJECT_ID: ${config.projectId}
-FLOWFORGE_TEAM_ID: ${config.teamId}
-host: ${config.host}
-port: ${config.port}
-base_url: ${config.base_url}
-driver:
-    type: memory
-    options:
-        root: var/root
-context:
-    type: ${config.context?.type}
-    quota: ${config.context?.quota || 1000}
-    options:
-        type: ${config.context?.options?.type || 'sqlite'}
-        storage: ${config.context?.options?.storage || '":memory:"'}
-`
-    const app = await require('@flowforge/file-server')
+    const FF_FS_TEST_CONFIG = {
+        FLOWFORGE_HOME: config.home || process.cwd(),
+        FLOWFORGE_PROJECT_ID: config.projectId,
+        FLOWFORGE_TEAM_ID: config.teamId,
+        host: config.host,
+        port: config.port,
+        base_url: config.base_url,
+        driver: {
+            type: 'memory',
+            options: {
+                root: 'var/root'
+            }
+        },
+        context: {
+            type: config.context?.type,
+            quota: config.context?.quota || 1000,
+            options: {
+                type: 'sqlite',
+                storage: 'ff-context.db'
+            }
+        }
+    }
+    if (config.context?.options?.type === 'postgres') {
+        FF_FS_TEST_CONFIG.context.options.storage = { ...config.context?.options }
+    }
+    process.env.FF_FS_TEST_CONFIG = YAML.stringify(FF_FS_TEST_CONFIG)
+    // const app = await require('@flowforge/file-server')
+    const app = await require('../../../../packages/flowforge-file-storage')
     return app
 }
 
